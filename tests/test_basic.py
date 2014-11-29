e827e7f834ecb3ad74ebe7a2957127c5a671bdd2
Thomas Kluyver <takowl@gmail.com>
2014-11-29 15:50:45 -0800

    Add basic test

---
+++ tests/test_basic.py
@@ -0,0 +1,16 @@
+from tabipy import Table, TableRow, TableCell, TableHeaderRow
+
+def test_simple():
+    # For now, this is little more than a smoke test to check that it's not
+    # erroring out.
+    t = Table(TableHeaderRow('a','b','c'),
+          (1,  2,  3),
+          (2,  4,  6),
+         )
+    
+    html = t._repr_html_()
+    assert '<th' in html
+    assert '<td' in html
+    
+    latex = t._repr_latex_()
+    assert r'\hline' in latex
