88bd7949b493acd3a4ce266cee9278b807c80ec6
Damon Allen <damontallen@gmail.com>
2015-02-10 06:05:38 -0500

    Added cell methos tests and moved Cell Method Test notebook to docs

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -49,4 +49,98 @@ def test_escape():
         cell = TableCell(inp)
         assert cell._repr_latex_() == expected
 
+def default_table():
+    "Returns the unmodified base table for testing"
+    t = Table((1,2,3),
+    (4,5,6),
+    (7,8,9))
+    return t
 
+def col_span_table():
+    "Returns the table used in the col_span tests"
+    t = default_table()
+    cell_1_1 = t.rows[0].cells[0]
+    cell_1_1.col_span = 2
+    return t
+
+def test_col_span_html():
+    "This tests that col_span works in html"
+    t = col_span_table()
+    #actual_col_span_html(t)
+    t1_html = t._repr_html_()
+    row_split = re.compile('<\s*tr\s*>')
+    lines = row_split.split(t1_html)
+    assert len(lines)==4
+    col_split = re.compile('>[\s\d\s]*<')
+    parts = col_split.split(lines[1])
+    cl_check = re.compile('colspan\s*=\s*"\s*2\s*"')
+    assert len(cl_check.findall(parts[0]))>0
+    
+def _actual_col_span_html(t):
+    "For testing at low level modifications and cell method"
+    t1_html = t._repr_html_()
+    row_split = re.compile('<\s*tr\s*>')
+    lines = row_split.split(t1_html)
+    assert len(lines)==4
+    col_split = re.compile('>[\s\d\s]*<')
+    parts = col_split.split(lines[1])
+    cl_check = re.compile('colspan\s*=\s*"\s*2\s*"')
+    assert len(cl_check.findall(parts[0]))>0
+    #print("pass")
+
+def test_col_span_latex():
+    "This tests that col_span works in latex"
+    t = col_span_table()  
+    #actual_col_span_latex(t)
+    t1_latex = t._repr_latex_()
+    row_split = re.compile(r'\\\\')
+    lines = row_split.split(t1_latex)
+    assert len(lines)==4
+    col_split = re.compile('&')
+    parts = col_split.split(lines[0])
+    cl_check = re.compile('\w*\\multicolumn\s*\{\s*2\s*}')
+    assert len(cl_check.findall(parts[0]))>0
+    
+def _actual_col_span_latex(t):
+    "For testing at low level modifications and cell method"
+    t1_latex = t._repr_latex_()
+    row_split = re.compile(r'\\\\')
+    lines = row_split.split(t1_latex)
+    assert len(lines)==4
+    col_split = re.compile('&')
+    parts = col_split.split(lines[0])
+    cl_check = re.compile('\w*\\multicolumn\s*\{\s*2\s*}')
+    assert len(cl_check.findall(parts[0]))>0
+    #print("pass")
+    
+def cell_method_col_span_table():
+    "Returns a table modified to test col_span using the cell method"
+    t = default_table()
+    t .cell((1,1), col_span = 2)
+    return t
+    
+def test_cell_method_col_span_html():
+    "This tests that col_span works in html"
+    t = cell_method_col_span_table()
+    #actual_col_span_html(t)
+    t1_html = t._repr_html_()
+    row_split = re.compile('<\s*tr\s*>')
+    lines = row_split.split(t1_html)
+    assert len(lines)==4
+    col_split = re.compile('>[\s\d\s]*<')
+    parts = col_split.split(lines[1])
+    cl_check = re.compile('colspan\s*=\s*"\s*2\s*"')
+    assert len(cl_check.findall(parts[0]))>0
+    
+def test_cell_method_col_span_html():
+    "This tests that col_span works in latex"
+    t = cell_method_col_span_table()
+    #actual_col_span_latex(t)
+    t1_latex = t._repr_latex_()
+    row_split = re.compile(r'\\\\')
+    lines = row_split.split(t1_latex)
+    assert len(lines)==4
+    col_split = re.compile('&')
+    parts = col_split.split(lines[0])
+    cl_check = re.compile('\w*\\multicolumn\s*\{\s*2\s*}')
+    assert len(cl_check.findall(parts[0]))>0
